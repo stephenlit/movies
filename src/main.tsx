ee7507e749dad0d2a558f1ac08533f85fdb3f55f
stephenlit <stephen.l@virtualtruro.com>
2025-01-07 14:36:19 -0400

    Added a stand alone start rating component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+//import './index.css'
+//import App from './App.tsx'
+import StarRating from "./components/hooks/StarRating";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        {/* <App /> */}
+        <StarRating />
+        <StarRating maxRating={10} />
+    </StrictMode>
+);
